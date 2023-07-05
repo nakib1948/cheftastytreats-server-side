8b055dcc617bb609b25929b1c3b4ad0a648d71dc
nakib1948 <hayathossain.jtp@gmail.com>
2023-07-06 00:04:34 +0600

    recipe data loading for client side complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 const chefs=require('./chefs.json')
+const recipes=require('./recipe.json')
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,6 +16,13 @@ app.get("/chefs", async (req, res) => {
     
     res.send(chefs);
   });
+
+  app.get('/recipes/:id', function(req, res) {
+    const id=req.params.id;
+   // console.log(typeof(id))
+    const recipe=recipes.filter(r=>r.chef_id==id)
+    res.send(recipe)
+});
 
 app.get("/", (req, res) => {
   res.send("Simple curd is running");
